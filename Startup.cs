f64b4ba697d3463a19ce46b8f9f26c17500b5645
Rikudou En Sof <rikudouensof@outlook.com>
2020-02-06 13:07:16 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CardinalTechOrgSite.Startup))]
+namespace CardinalTechOrgSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
